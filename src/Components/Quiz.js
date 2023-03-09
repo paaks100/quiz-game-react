9fd6ab4ccb444df944697de92d8aad9b335bcfaf
paaks100 <pkkodua@gmail.com>
2023-03-09 11:15:43 +0000

    quiz questions and options

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,7 +1,21 @@
+import { useState } from "react"
 import "../App.css"
+import { Questions } from "../Helpers/QuestionBank";
 
 export const Quiz = () => {
+    const [currQuestion, setCurrQuestion] = useState(0);
+    const [optionChosen, setOptionChosen] = useState("");
+
     return (
-        <div className="square">Quiz</div>
+        <div className="Quiz">
+            <h1>{Questions[currQuestion].prompt}</h1>
+            <div className="options">
+                <button> {Questions[currQuestion].optionA} </button>
+                <button> {Questions[currQuestion].optionB} </button>
+                <button> {Questions[currQuestion].optionC} </button>
+                <button> {Questions[currQuestion].optionD} </button>
+            </div>
+            <button> Next {">>>"}</button>
+        </div>
     )
 }
---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,42 @@
+export const Questions = [
+    {
+        prompt: "What is 1+3?",
+        optionA: "13",
+        optionB: "4",
+        optionC: "2",
+        optionD: "5",
+        answer: "optionC"
+    },
+    {
+        prompt: "Which is a streaming platform?",
+        optionA: "Twitter",
+        optionB: "Facebook",
+        optionC: "CouchTuner",
+        optionD: "Instagram",
+        answer: "optionC"
+    },
+    {
+        prompt: "Who is the current US president?",
+        optionA: "Barack Obama",
+        optionB: "Donald Trump",
+        optionC: "George Bush",
+        optionD: "Joe Biden",
+        answer: "optionD"
+    },
+    {
+        prompt: "What is the world's most spoken language?",
+        optionA: "Spanish",
+        optionB: "English",
+        optionC: "Chinese",
+        optionD: "French",
+        answer: "optionB"
+    },
+    {
+        prompt: "What team does Cristiano Ronaldo play for?",
+        optionA: "Manchester United",
+        optionB: "Real Madrid",
+        optionC: "Bayern Munich",
+        optionD: "Arsenal",
+        answer: "optionA"
+    }
+]
